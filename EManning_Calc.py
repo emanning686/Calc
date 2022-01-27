573826f5c8c6732c434500b14f31b57155d40bf6
emanning686 <98548472+emanning686@users.noreply.github.com>
2022-01-27 17:19:55 -0500

    Update EManning_Calc.py

--- EManning_Calc.py
+++ EManning_Calc.py
@@ -5,6 +5,7 @@ i = 0
 
 def inputscript():
     global currentnum
+    global file1
     tempinput = input("Input (number, clear, end): ")
     try:
         currentnum = float(tempinput)
@@ -15,9 +16,46 @@ def inputscript():
             file1.close()
             exit()
         else:
+            file1.write(str(tempinput) + "\n")
             file1.write("error \n")
+            file1.close()
+            file1 = open("calcdata.txt", "a+")
             print("error")
             inputscript()
+
+def actionscript():
+    global action
+    global num2
+    global currentnum
+    global file1
+    action = input("Action (+, -, *, /, clear, end): ")
+    try:
+        actionnumber = float(action)
+    except ValueError:
+        if action == "clear":
+            skip
+        elif action == "end":
+            skip
+        else:
+            tempinput2 = input("Number: ")
+            try:
+                num2 = float(tempinput2)
+                file1.write(str(action) + " " + str(num2) + "\n")
+            except ValueError:
+                if tempinput2 == "clear":
+                    clearscript()
+                elif tempinput2 == "end":
+                    file1.close()
+                    exit()
+                else:
+                    file1.write(str(action) + " " + str(tempinput2) + "\n")
+                    file1.write("error \n")
+                    file1.close()
+                    file1 = open("calcdata.txt", "a+")
+                    print("error")
+                    inputscript()
+                    actionscript()
+    
 
 def clearscript():
     global currentnum
@@ -31,29 +69,7 @@ file1.write(str(currentnum) + "\n")
 file1.close()
 while i == 0:
     file1 = open("calcdata.txt", "a+")
-    action = input("Action (+, -, *, /, clear, end): ")
-    try:
-        actionnumber = float(action)
-    except ValueError:
-        if action == "clear":
-            skip
-        elif action == "end":
-            skip
-        else:
-            tempinput2 = float(input("Number: "))
-            try:
-                num2 = float(tempinput2)
-            except ValueError:
-                if tempinput2 == "clear":
-                    clearscript()
-                elif tempinput2 == "end":
-                    file1.close()
-                    exit()
-                else:
-                    file1.write("error \n")
-                    print("error")
-                    inputscript()
-    file1.write(str(action) + " " + str(num2) + "\n")
+    actionscript()
     if action == "+":
         currentnum += num2
         print("output: " + str(currentnum))
